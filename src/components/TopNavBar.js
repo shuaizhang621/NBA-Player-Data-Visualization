833483f6f1088b0d197e29e17e473f20eb33520d
shuaizhang621 <sz1950@nyu.edu>
2018-04-07 18:02:20 -0400

    add header

--- src/components/TopNavBar.js
+++ src/components/TopNavBar.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from '../assets/image/logo.svg';
+import logo from '../assets/image/nba-log.svg';
 
 export class Header extends React.Component {
     render() {
         return (
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <h1 className="App-title">Welcome to React</h1>
             </header>
         );
     }
